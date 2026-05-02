61ac94a57962266bc5c8981d7140e1455ab1e85b
Sai-Janjirala <saijanjirala5411@gmail.com>
2026-05-02 16:28:18 +0530

    chore: configure Vercel routing with redirects and updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/',
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': process.env.VITE_API_URL || 'http://localhost:5000'
     }
   },
   build: {
